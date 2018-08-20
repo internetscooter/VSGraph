b01dc51ed1a6b4420c06fe6d71b75ea2391f4b17
Paul McIntosh <paul.mcintosh@internetscooter.com>
2018-08-21 09:09:21 +1000

    some bad code before a refactor

--- extension.js
+++ extension.js
@@ -8,12 +8,12 @@ function activate(context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "scribble" is now active!');
+    console.log('Congratulations, your extension "vsgraph" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    let disposable = vscode.commands.registerCommand('extension.vsgraph', function () {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
