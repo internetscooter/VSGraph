064a31b31f4f50f17eee89c647ba60fb4c928bc9
Paul McIntosh <paul.mcintosh@internetscooter.com>
2018-08-21 12:54:48 +1000

    javascript webview

--- extension.js
+++ extension.js
@@ -1,22 +1,24 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vsgraph" is now active!');
+    console.log('"vsgraph" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.vsgraph', function () {
         // The code you place here will be executed every time your command is executed
-
+        const panel = vscode.window.createWebviewPanel(
+            'vsgraph',
+            "vsgraph",
+            vscode.ViewColumn.One,
+            {}
+        )
         // Display a message box to the user
+        panel.webview.html = getWebviewContent();
         vscode.window.showInformationMessage('Hello Graphs! Not!');
     });
 
@@ -28,3 +30,18 @@ exports.activate = activate;
 function deactivate() {
 }
 exports.deactivate = deactivate;
+
+
+function getWebviewContent() {
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+</body>
+</html>`;
+}
